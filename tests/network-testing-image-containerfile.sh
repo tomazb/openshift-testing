83585752a303156cb8e11975615b9d50754b0b6d
Tomaz <1279460+tomazb@users.noreply.github.com>
2026-04-30 16:43:09 +0200

    Add network toolbox static tests

--- tests/network-testing-image-containerfile.sh
+++ tests/network-testing-image-containerfile.sh
@@ -7,6 +7,39 @@ CONTAINERFILE="$REPO_ROOT/network-testing-image/Containerfile"
 grep -Fxq "FROM registry.access.redhat.com/ubi9/ubi:9.7" "$CONTAINERFILE"
 grep -Fq 'org.opencontainers.image.source="https://github.com/tomazb/openshift-testing"' "$CONTAINERFILE"
 grep -Eq '^[[:space:]]+unzip[[:space:]]+\\$' "$CONTAINERFILE"
+
+for package in \
+  bash-completion \
+  bind-utils \
+  whois \
+  netperf \
+  qperf \
+  httpd-tools \
+  jq \
+  nmap \
+  wireshark-cli \
+  ethtool \
+  s3fs-fuse \
+  fio; do
+  if ! grep -Eq "^[[:space:]]+${package}[[:space:]]+\\\\$" "$CONTAINERFILE"; then
+    echo "missing expected package in Containerfile: $package" >&2
+    exit 1
+  fi
+done
+
+grep -Fxq "ARG OPENSHIFT_CLIENT_VERSION=4.19.12" "$CONTAINERFILE"
+grep -Fxq "ARG STEP_CLI_VERSION=0.30.2" "$CONTAINERFILE"
+grep -Fxq "ARG YQ_VERSION=v4.53.2" "$CONTAINERFILE"
+
+grep -Fq 'openshift-client-linux-${OC_ARCH}-rhel9-${OPENSHIFT_CLIENT_VERSION}.tar.gz' "$CONTAINERFILE"
+grep -Fq 'https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${OPENSHIFT_CLIENT_VERSION}' "$CONTAINERFILE"
+grep -Fq 'https://github.com/smallstep/cli/releases/download/v${STEP_CLI_VERSION}' "$CONTAINERFILE"
+grep -Fq 'https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}' "$CONTAINERFILE"
+
+for completion in oc kubectl rclone step yq; do
+  grep -Fq "/etc/bash_completion.d/$completion" "$CONTAINERFILE"
+done
+
 grep -Eq '^ARG RCLONE_VERSION=v[0-9]+\.[0-9]+\.[0-9]+$' "$CONTAINERFILE"
 
 if grep -Fq "rclone-current" "$CONTAINERFILE"; then
